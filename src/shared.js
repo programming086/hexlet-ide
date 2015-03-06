0980a671196ab536eb937d9f888d74a653383870
Vagin Alexander <laplugin73@gmail.com>
2015-03-06 11:35:59 +0000

    add alphabet sort

--- src/shared.js
+++ src/shared.js
@@ -7,6 +7,8 @@ module.exports = {
         return -1;
       } else if (b.type === "directory" && a.type === "file") {
         return 1;
+      } else if (a.type === "directory" && b.type === "directory") {
+        return a.name.localeCompare(b.name);
       } else {
         // FIXME sort names
         return 0;
