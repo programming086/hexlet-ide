61341c1a9e3b56c20af21655f2566404548bbe9b
alexeyfrank <frank@undev.ru>
2015-03-17 22:37:06 +0300

    hotfixes

--- src/editor/components/PopupBox.js
+++ src/editor/components/PopupBox.js
@@ -31,7 +31,6 @@ export default React.createClass({
     return (
       <div>
         <div className="modal fade in" style={{ display: "block" }}>
-          <div className="modal-backdrop fade in"></div>
           {this.renderCurrentModal()}
         </div>
       </div>
