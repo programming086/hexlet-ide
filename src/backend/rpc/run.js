9b76b15a606cb2c397deeceb26f393f07d9c0ce4
alexeyfrank <alexeyfrank@gmail.com>
2015-04-14 15:43:01 +0300

    fix process killing

--- src/backend/rpc/run.js
+++ src/backend/rpc/run.js
@@ -4,41 +4,68 @@ var cp = require("child_process");
 var when = require("when");
 
 module.exports = function(options) {
+  var childs = {
+  };
+
   return {
-
     exec: function(command) {
-      var deferred = when.defer();
-
-      if (this.child) {
-        this.child.kill("SIGKILL");
-      }
-
       var socket = this.clientSocket;
       var arr = command.split(" ");
+      var deferred = when.defer();
+
+      var currentChild = _.find(childs, { current: true });
+
+      if (currentChild) {
+        currentChild.state = "killed_manually";
+        currentChild.current = false;
+
+        process.kill(-currentChild.process.pid);
+
+        // currentChild.process.stdin.pause();
+        // currentChild.process.stdout.pause();
+        // currentChild.process.kill();
+        console.log("Kill child with pid: " + currentChild.process.pid);
+      }
 
       console.log("Run command: ", arr.join(" "), { cwd: options.rootDir });
+      var proc = cp.spawn(arr[0], _.tail(arr), { cwd: options.rootDir, detached: true });
 
-      this.child = cp.spawn(arr[0], _.tail(arr), { cwd: options.rootDir });
+      var child = {
+        state: "started",
+        process: proc,
+        current: true
+      };
 
-      this.child.on("exit", function(code, signal) {
-        var data = {
-          code: code,
-          signal: signal
-        };
-        socket.emit("run.finish", data);
+      proc.on("exit", function(code, signal) {
+        if (child.state !== "killed_manually") {
+          var data = {
+            code: code,
+            signal: signal
+          };
+          socket.emit("run.finish", data);
 
-        deferred.resolve(data);
+          deferred.resolve(data);
+
+          child.state = "finished";
+          child.current = false;
+        }
       });
 
-      this.child.stderr.on("data", function(chunk) {
-        console.log("Run stderr data: ", chunk);
-        socket.emit("run.progress", chunk.toString());
+      proc.stderr.on("data", function(chunk) {
+        if (child.state !== "killed_manually") {
+          console.log("Run stderr data: ", chunk);
+          socket.emit("run.progress", chunk.toString());
+        }
       });
 
-      this.child.stdout.on("data", function(chunk) {
-        console.log("Run stdout data: ", chunk);
-        socket.emit("run.progress", chunk.toString());
+      proc.stdout.on("data", function(chunk) {
+        if (child.state !== "killed_manually") {
+          console.log("Run stdout data: ", chunk);
+          socket.emit("run.progress", chunk.toString());
+        }
       });
+
+      childs[proc.pid] =  child;
 
       return deferred.promise;
     }
