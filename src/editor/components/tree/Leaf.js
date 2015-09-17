829a3024be56770048dba12fb362c3be114d6dc3
Ilya Shakirov <aelaau@gmail.com>
2015-09-17 12:20:12 +0300

    Refactor Tree and Leaf

--- src/editor/components/tree/Leaf.js
+++ src/editor/components/tree/Leaf.js
@@ -1,33 +1,30 @@
 import _ from "lodash";
-import React, {Component} from "react/addons";
 
 import {openFile} from "editor/actions/TreeActions";
 
-class Leaf extends Component<{}, {}, {}> {
-  handleOpenFile(leaf, e) {
-    e.stopPropagation();
-    openFile(leaf);
-  }
+var handleOpenFile = (leaf, e) => {
+  e.stopPropagation();
+  openFile(leaf);
+}
 
-  handleContextMenu(leaf, e) {
-    this.props.handleContextMenu(e, leaf);
-  }
+var handleContextMenu = (leaf, props, e) => {
+  props.handleContextMenu(e, leaf);
+}
 
-  render() {
-    var leaf = this.props.leaf;
+const Leaf = (props) => {
+    var leaf = props.leaf;
     if (!leaf) return null;
 
     return (
       <li className="tree-item" data-template="treeitem" role="treeitem"
-          onContextMenu={this.handleContextMenu.bind(this, leaf)}
-          onClick={this.handleOpenFile.bind(this, leaf)}>
+          onContextMenu={handleContextMenu.bind(this, leaf, props)}
+          onClick={handleOpenFile.bind(this, leaf)}>
         <a href="#" className="tree-item-name">
           <span className="glyphicon icon-file glyphicon-file"></span>
           <span className="tree-label">{leaf.name}</span>
         </a>
       </li>
     );
-  }
 };
 
 export default Leaf;
