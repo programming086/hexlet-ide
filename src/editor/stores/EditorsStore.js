0e80ea81bdd4eaf88852fa61101df684498e10cf
Ilya Shakirov <aelaau@gmail.com>
2015-09-17 11:37:41 +0300

    Change var to const

--- src/editor/stores/EditorsStore.js
+++ src/editor/stores/EditorsStore.js
@@ -53,9 +53,9 @@ class EditorsStore extends ReduceStore {
 
   static handlers = {
     [ActionTypes.TREE_OPEN_FILE]: function(state, action) {
-      var item = action.item;
-      var content = action.content;
-      var editor = this.getAll().find((v) => { return v.get('id') == item.id});
+      const item = action.item;
+      const content = action.content;
+      const editor = this.getAll().find((v) => { return v.get('id') == item.id});
 
       if (!editor) {
         return state.update('editors', (v) => {
@@ -72,7 +72,7 @@ class EditorsStore extends ReduceStore {
             lastSavingAt: new Date()
           }))})
       } else {
-        var idx = this.getAll().indexOf(editor);
+        const idx = this.getAll().indexOf(editor);
 
         return state.update('editors', (v) => {
           return v.map((s) => {
@@ -82,7 +82,7 @@ class EditorsStore extends ReduceStore {
     },
 
     [ActionTypes.EDITORS_EDIT_CURRENT]: function(state, action) {
-      var idx = this.getAll().findIndex((v) => { return v.get('id') === action.item.id});
+      const idx = this.getAll().findIndex((v) => { return v.get('id') === action.item.id});
 
       return state.setIn(['editors', idx, "content"], action.content)
       .setIn(['editors', idx, "dirty"], true)
@@ -90,19 +90,19 @@ class EditorsStore extends ReduceStore {
     },
 
     [ActionTypes.EDITORS_SAVING_CURRENT]: function(state, action) {
-      var idx = this.getAll().findIndex((v) => { return v.get('id') === action.item.id});
+      const idx = this.getAll().findIndex((v) => { return v.get('id') === action.item.id});
 
       return state.setIn(['editors', idx, "lastSavingAt"], new Date());
     },
 
     [ActionTypes.EDITORS_SAVE_CURRENT]: function(state, action) {
-      var idx = this.getAll().findIndex((v) => { return v.get('id') === action.item.id});
+      const idx = this.getAll().findIndex((v) => { return v.get('id') === action.item.id});
 
       return state.setIn(['editors', idx, "dirty"], false);
     },
 
     [ActionTypes.EDITORS_MAKE_CURRENT]: function(state, action) {
-      var idx = this.getAll().findIndex((v) => { return v.get('id') === action.id});
+      const idx = this.getAll().findIndex((v) => { return v.get('id') === action.id});
 
       return state.update('editors', (v) => {
         return v.map((s) => {
@@ -112,10 +112,10 @@ class EditorsStore extends ReduceStore {
     },
 
     [ActionTypes.KEY_CTRL_OPEN_SQUARE_BR]: function(state, action) {
-      var editors = this.getAll();
+      const editors = this.getAll();
       if (editors.size === 0) return;
 
-      var idx = editors.indexOf(this.getCurrent());
+      const idx = editors.indexOf(this.getCurrent());
       var newIdx = idx;
       if (idx == 0) {
         newIdx = editors.size - 1;
@@ -131,10 +131,10 @@ class EditorsStore extends ReduceStore {
     },
 
     [ActionTypes.KEY_CTRL_CLOSE_SQUARE_BR]: function(state, action) {
-      var editors = this.getAll();
+      const editors = this.getAll();
       if (editors.size === 0) return;
 
-      var idx = editors.indexOf(this.getCurrent());
+      const idx = editors.indexOf(this.getCurrent());
       var newIdx = idx;
       if (idx == editors.size - 1) {
         newIdx = 0;
@@ -150,12 +150,12 @@ class EditorsStore extends ReduceStore {
     },
 
     [ActionTypes.EDITORS_CLOSE]: function(state, action) {
-      var editors = this.getAll();
-      var removedEditor = editors.find((v) => { return v.get('id') == action.id});
-      var otherEditors = editors.filter((t) => { return t.get('id') !== action.id; });
+      const editors = this.getAll();
+      const removedEditor = editors.find((v) => { return v.get('id') == action.id});
+      const otherEditors = editors.filter((t) => { return t.get('id') !== action.id; });
 
       if (removedEditor.get('current') && otherEditors.size > 0) {
-        var idx = otherEditors.findLastIndex(() => {return true})
+        const idx = otherEditors.findLastIndex(() => {return true})
         return state.update('editors', (v) => {
           return v.filter((t) => { return t.get('id') !== action.id })})
           .setIn(['editors', idx, "current"], true);
@@ -166,7 +166,7 @@ class EditorsStore extends ReduceStore {
     },
 
     [ActionTypes.EDITORS_SHOW_RUN_VIEW]: function(state, action) {
-      var editors = this.getAll();
+      const editors = this.getAll();
       return state.update('editors', (v) => {
         return v.map((s) => {
           return s.set('current', false);
@@ -174,7 +174,7 @@ class EditorsStore extends ReduceStore {
     },
 
     [ActionTypes.IDE_RUN]: function(state, action) {
-      var editors = this.getAll();
+      const editors = this.getAll();
       return state.update('editors', (v) => {
         return v.map((s) => {
           return s.set('current', true);
@@ -182,16 +182,16 @@ class EditorsStore extends ReduceStore {
     },
 
     [ActionTypes.TREE_REMOVE]: function(state, action) {
-      var currentEditor = this.getCurrent();
-      var editors = this.getAll();
-      var removedFiles = action.removedFiles;
+      const currentEditor = this.getCurrent();
+      const editors = this.getAll();
+      const removedFiles = action.removedFiles;
 
-      var removedEditors = editors.filter((t) => { return !_.contains(removedFiles, t.get('id')); });
-      var needSelectNewEditor = removedEditors.contains(currentEditor) && removedEditors.size > 0;
+      const removedEditors = editors.filter((t) => { return !_.contains(removedFiles, t.get('id')); });
+      const needSelectNewEditor = removedEditors.contains(currentEditor) && removedEditors.size > 0;
       var idx = -1;
 
       if (needSelectNewEditor) {
-        var editor = removedEditors.last();
+        const editor = removedEditors.last();
         idx = editors.indexOf(editor);
       }
 
