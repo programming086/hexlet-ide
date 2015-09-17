22c74ee8480c466719eca31b67a70b51fd4321a3
Ilya Shakirov <aelaau@gmail.com>
2015-09-17 10:00:20 +0300

    Fix Actions

--- src/editor/actions/EditorsActions.js
+++ src/editor/actions/EditorsActions.js
@@ -5,44 +5,26 @@ import TreeStore from "editor/stores/TreeStore";
 import rpc from "editor/lib/RpcClient";
 
 export function closeEditor(editor) {
-  dispatch({
-    actionType: ActionTypes.EDITORS_CLOSE,
-    id: editor.id
-  });
+  dispatch(ActionTypes.EDITORS_CLOSE, { id: editor.id });
 }
 
 export function makeCurrent(editor) {
-  dispatch({
-    actionType: ActionTypes.EDITORS_MAKE_CURRENT,
-    id: editor.id
-  });
+  dispatch(ActionTypes.EDITORS_MAKE_CURRENT, { id: editor.id });
 }
 
 export function save(editor) {
   const path = TreeStore.getPathById(editor.id);
-  dispatch({
-    actionType: ActionTypes.EDITORS_SAVING_CURRENT,
-    id: editor.id
-  });
+  dispatch(ActionTypes.EDITORS_SAVING_CURRENT, { id: editor.id });
 
   return rpc.getClient().fs.write(path, editor.content).then(_ => {
-    dispatch({
-      actionType: ActionTypes.EDITORS_SAVE_CURRENT,
-      id: editor.id
-    });
+    dispatch(ActionTypes.EDITORS_SAVE_CURRENT, { id: editor.id });
   });
 }
 
 export function edit(editor, content) {
-  dispatch({
-    actionType: ActionTypes.EDITORS_EDIT_CURRENT,
-    id: editor.id,
-    content: content
-  });
+  dispatch(ActionTypes.EDITORS_EDIT_CURRENT, { id: editor.id, content: content });
 }
 
 export function showRunView() {
-  dispatch({
-    actionType: ActionTypes.EDITORS_SHOW_RUN_VIEW
-  });
+  dispatch(ActionTypes.EDITORS_SHOW_RUN_VIEW);
 };
